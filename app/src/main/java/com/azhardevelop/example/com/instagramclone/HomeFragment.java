74b0bad76620c9f5cb38424b20862a1bab86bfeb
Siapa Kamu Gw Liat Lo <Manusia@Moba-Kok-Analog.local>
2019-03-18 22:15:11 +0700

    Kepooo

---
+++ app/src/main/java/com/azhardevelop/example/com/instagramclone/HomeFragment.java
@@ -0,0 +1,111 @@
+package com.azhardevelop.example.com.instagramclone;
+
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    private ArrayList<HashMap<String, String>> postData;
+    private String url;
+    TextView txtUsername, txtUsercap, txtCaption;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        txtUsername = view.findViewById(R.id.txt_username);
+        txtUsercap = view.findViewById(R.id.txt_usernamecap);
+        txtCaption = view.findViewById(R.id.txt_caption);
+
+        postData = new ArrayList<HashMap<String, String>>();
+        url = "https://mzdzharserver.000webhostapp.com/SMPIDN/webdatabase/api_tampilpost.php";
+        showData();
+    }
+
+    private void showData() {
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Loading...");
+        progressDialog.setMessage("Please Wait...");
+        progressDialog.show();
+
+        JsonObjectRequest myRequest = new JsonObjectRequest(Request.Method.GET
+                , url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                progressDialog.dismiss();
+                Toast.makeText(getActivity(), response.toString(), Toast.LENGTH_LONG).show();
+                try {
+                    JSONArray jsonArray = response.getJSONArray("post");
+                    for (int a = 0; a < jsonArray.length(); a++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(a);
+                        HashMap<String, String> rowData = new HashMap<String, String>();
+                        rowData.put("username", jsonObject.getString("username"));
+                        rowData.put("caption", jsonObject.getString("caption"));
+                        postData.add(rowData);
+                    }
+                    // menampilkan datanya di komponen
+                    txtUsername.setText(postData.get(1).get("username"));
+                    txtUsercap.setText(postData.get(1).get("username"));
+                    txtCaption.setText(postData.get(1).get("caption"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // menjalankan request
+        RequestQueue myRequestQueue = Volley.newRequestQueue(getActivity());
+        myRequestQueue.add(myRequest);
+    }
+}
+
+
---
+++ app/src/main/java/com/azhardevelop/example/com/instagramclone/MainActivity.java
@@ -0,0 +1,62 @@
+package com.azhardevelop.example.com.instagramclone;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+        toolbar.setTitle("Home");
+        loadFragment(new HomeFragment());
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_add:
+                    toolbar.setTitle("Add");
+                    fragment = new AddFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.navigation_home:
+                    toolbar.setTitle("My Gifts");
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.navigation_profile:
+                    toolbar.setTitle("Profile");
+                    fragment = new ProfileFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
